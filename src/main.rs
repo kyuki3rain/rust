487042aa7cc0c0278d9431fbdd078f0fa6849248
kyuki3rain <kyuki3rain@gmail.com>
2023-04-19 09:40:20 +0000

    testのクオリティを上げる git push] git commit -m testのクオリティを上げる

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::env;
-mod compile;
-mod token;
-mod lexer;
-mod parser;
 mod ast;
 mod compiler;
+mod lexer;
+mod parser;
+mod token;
 
 // c-compiler
 fn main() {
@@ -25,33 +24,45 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use std::process::Output;
-    use std::{process::Command, fs::File};
+    use std::fs;
     use std::io::Write;
-    
+    use std::process::Output;
+    use std::{fs::File, process::Command};
+
+    extern crate rand;
+    use rand::Rng;
+
     use super::*;
 
     fn execute(program: &str) -> Output {
+        let mut rng = rand::thread_rng();
+        execute_with_filename(program, &rng.gen::<u32>().to_string())
+    }
+
+    fn execute_with_filename(program: &str, filename: &str) -> Output {
+        let asm_path = String::new() + "./tmp/" + filename + ".s";
+        let exe_path = String::new() + "./tmp/" + filename + ".out";
+
         let output = Command::new(env!("CARGO"))
-        .args(vec!["run", program])
-        .output()
-        .expect("failed to compile");
+            .args(vec!["run", program])
+            .output()
+            .expect("failed to compile");
 
         let mut path = env::current_dir().unwrap();
-        path.push("tmp");
-        path.push("test1.s");
+        path.push(&asm_path);
         let mut file = File::create(path).unwrap();
         write!(file, "{}", String::from_utf8_lossy(&output.stdout)).unwrap();
         file.flush().unwrap();
-        
-        Command::new("cc")
-        .args(&["-o", "./tmp/test1.out", "./tmp/test1.s"])
-        .output()
-        .expect("failed to build");
 
-        let output = Command::new("./tmp/test1.out")
-        .output()
-        .expect("failed to execute");
+        Command::new("cc")
+            .args(&[&asm_path, "-o", &exe_path])
+            .output()
+            .expect("failed to build");
+
+        let output = Command::new(&exe_path).output().expect("failed to execute");
+
+        fs::remove_file(asm_path).expect("failed to remove asm file");
+        fs::remove_file(exe_path).expect("failed to remove exe file");
 
         output
     }
@@ -69,5 +80,20 @@ mod test {
         let program = "123";
         let output = execute(program);
         assert_eq!(output.status.code().unwrap(), 123);
+    }
+
+    #[test]
+    fn test_plusminus() {
+        let program = "1+2";
+        let output = execute(program);
+        assert_eq!(output.status.code().unwrap(), 3);
+
+        let program = "1-2";
+        let output = execute(program);
+        assert_eq!(output.status.code().unwrap(), 255);
+
+        let program = "114+41-136";
+        let output = execute(program);
+        assert_eq!(output.status.code().unwrap(), 19);
     }
 }
